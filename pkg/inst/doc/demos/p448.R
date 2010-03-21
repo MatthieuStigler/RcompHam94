e7a8ea9571fee3a76973eb6e6dbb8c1a51e4f3cd
Matthieu Stigler <Matthieu.Stigler@gmail.com>
2010-03-21 13:59:48 +0100

    Remove most machine generated files from git index

--- pkg/inst/doc/demos/p448.R
+++
@@ -1,8 +0,0 @@
-###################################################
-### chunk number 1: 
-###################################################
-data(gnptbill,package = "Ham94")
-print( fdGPH(gnptbill$GNP) )
-print( fdGPH(gnptbill$TBILL) )
-
-
