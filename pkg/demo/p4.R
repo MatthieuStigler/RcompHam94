b37ae30435a9e1ec0402cd3d06f7155061f4c613
Robert Bell <robertkevinbell.com>
2010-05-17 07:25:52 -0400

    Update demo directory version of p4.R

--- pkg/demo/p4.R
+++ pkg/demo/p4.R
@@ -14,13 +14,9 @@ print( y[2:T,1] )
 print( phis[[1]]^seq(0,T-2) )
 
 
-screens <- split.screen( figs=c(2,2) )
+par( mfrow=c(2,2) )
 for ( i in 1:length(phis) )
-{
-  screen( n = screens[[i]], new = TRUE )
   plot( 1:T,y[,i], type = "h", xlab=paste("f = ",phis[[i]]), ylab="",
     font.lab = 5, cex=.5, lwd=5,lend=1,ylim=c(-max(y[,i]),max(y[,i])))
-}
-close.screen(all=TRUE)
 
 
